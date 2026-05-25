8d720859b9ffe87048d4972052cf79c383e93ca3
KET_KAT <fclm489@gmail.com>
2026-05-25 11:22:21 +0800

    Update console rpg labyrinth.py

--- console rpg labyrinth.py
+++ console rpg labyrinth.py
@@ -37,37 +37,31 @@ while True:
 
 room = 1
 key = False
-print("ты просыпаешься в каком-то лаберинте ты видишь две развили (влево, вправо)")
-
 stats_player = {
     "HP": 100,
     "AP": 100  # вдохновился механикой из блек солс
 }
 monster_HP = 150
-count_room_1 = 0
-count_room_2 = 0
 random_room = ["влево","вправо"]
 count_ded = 0
 anti_cheat1 = random.choice(random_room)
 anti_cheat2 = random.choice(random_room)
 anti_cheat3 = random.choice(random_room)
+print("ты просыпаешься в каком-то лаберинте ты видишь две развили (влево, вправо)")
 
 while True:
     room_input = input("куда пойдёшь: ").lower().strip()
     if room == 1:
         if  room_input == anti_cheat1:
             room = 2
-            print("ты во второй комнате. ты видишь два пути (влево, вправо) ")
+            print("ты во второй комнате. ты видишь два пути (влево, вперёд, вправо)")
         else:
             monster_hit = random.randint(1,10)
-            print(f"ты увидел стену где кровью написано: БЕГИ ПОКА ОН ТЕБЯ НЕ НАСТИГ! \n Вы сильно испугались что не увидели огромную крысу которая вас укусила и нанесла {monster_hit}")
+            print(f"ты увидел стену где кровью написано: БЕГИ ПОКА ОН ТЕБЯ НЕ НАСТИГ! \nВы сильно испугались что не увидели огромную крысу которая вас укусила и нанесла {monster_hit} урона")
     elif room == 2:
         if room_input == anti_cheat2:
             room = 3
-            if count_room_2 == 0:
-                anti_cheat = random.choices(random_room)
-                count_room_2 = 1
-                print("Ты стоишь во второй комнате. Куда теперь? ()")
+            print("Ты стоишь во третьей комнате. Куда теперь? (влево, вправо)")
         elif room_input in ["вперёд", "прямо"]:
             os.system("cls")
             print("вы видите гоблина который на вас наподает")
@@ -105,13 +99,15 @@ while True:
                         else:
                             Crete = random.randint(1,50)
                             if Crete == 30:
-                                print("вы нанесли критический удар!")
+                                print("вы нанесли критический удар на 100 урона!")
                                 monster_HP -= 100
                                 stats_player["AP"] -= 100
                             else:
                                 hit = random.randint(10,50)
-                                print(f"вы нанесли {hit}")
+                                print(f"вы нанесли {hit} урона")
                                 stats_player["AP"] -= 40
+                        
+                        print(f"Ты заблокировал часть урона! Вам нанесли: {hit_sum} урона")
                     else:
                         print("Не хватает AP для удара! Ты промахнулся.")
                         monster_hit = random.randint(10, 30)
@@ -120,19 +116,19 @@ while True:
                 elif player_actions == 2:
                     if stats_player["AP"] >= 10:
                         monster_hit = random.randint(10, 30)
-                        shield_block_percent = 13 
+                        shield_block_percent = 13
                         
                         hit_sum = int(monster_hit * (1 - shield_block_percent / 100))
                         
                         stats_player["HP"] -= hit_sum
                         stats_player["AP"] -= 10
                         
-                        print(f"Ты заблокировал часть урона! Вам нанесли: {hit_sum} HP")
+                        print(f"Ты заблокировал часть урона! Вам нанесли: {hit_sum} урона")
                     else:
                         print("Не хватает AP для переката! Ты неуклюжа упал.")
                         monster_hit = random.randint(10, 30)
                         stats_player["HP"] -= monster_hit
-                        print(f"Монстр ударил на {monster_hit} HP!")
+                        print(f"Монстр ударил на {monster_hit} урона")
 
                 elif player_actions == 3:
                     if stats_player["AP"] >= 5:
@@ -140,7 +136,7 @@ while True:
                         if dodging == 2:
                             monster_hit = random.randint(10,30)
                             stats_player["HP"] -= monster_hit
-                            print(f"вам не удалось уклонится вам нанесли: {monster_hit}")
+                            print(f"вам не удалось уклонится вам нанесли: {monster_hit} урона")
                             stats_player["AP"] -= 5
                         else:
                             print("вы удачно уклонились!")
@@ -149,19 +145,19 @@ while True:
                         print("Не хватает AP для укланения! Ты был настолько медленым во время переката что гоблин подумал что убагаешь.")
                         monster_hit = random.randint(15, 30)
                         stats_player["HP"] -= monster_hit
-                        print(f"Монстр ударил на {monster_hit} HP!")
+                        print(f"Монстр ударил на {monster_hit} урона")
                 elif player_actions == 4:
                     monster_hit = random.randint(10,30)
                     stats_player["HP"] -= monster_hit
                     recovery_AP = random.randint(10,40)
                     stats_player["AP"] = min(100, stats_player["AP"] + recovery_AP)
-                    print(f"вы востановили себе AP: {recovery_AP}")
+                    print(f"вы востановили себе AP: {recovery_AP} | но вам нанесли {monster_hit} урона")
                 else:
                     monster_hit = random.randint(10,30)
                     stats_player["HP"] -= monster_hit
                     recovery_AP = random.randint(10,40)
                     stats_player["AP"] = min(100, stats_player["AP"] + recovery_AP)
-                    print(f"число вне диапозона монстр нанёс вам урон: {monster_hit} когда вы думали что сделать | но вы востановили себе AP: {recovery_AP}")
+                    print(f"число вне диапозона монстр нанёс вам {monster_hit} урона | когда вы думали что сделать но вы востановили себе AP: {recovery_AP}")
             if count_ded == 1:
                 break
             print("Ты видишь тупик, но на полу лежит ключ!")
